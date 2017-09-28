c77c8ac43d0b546d0b32741842e515032628048f
Ramsay Lanier <rlanier@ALT8AG8WL.local>
2017-09-27 20:06:40 -0400

    add config for dev database location

--- database/index.js
+++ database/index.js
@@ -1,7 +1,12 @@
 import mongoose from 'mongoose'
+import Config from 'config'
+
+const dbHost = Config.get('database.host')
+console.log(dbHost)
+
 const connection = mongoose.connect('mongodb://localhost/testrssschema_dev', {
   useMongoClient: true,
-  promiseLibrary: global.Promise 
+  promiseLibrary: global.Promise
 });
 
 export default connection
